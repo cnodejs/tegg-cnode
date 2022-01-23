5001110679044afb7cfbd1844e9b9336fb17aed5
Suyi <thonatos.yang@gmail.com>
2022-01-23 15:37:37 +0800

    feat: update auth validator

--- app/api/controller/AuthController.ts
+++ app/api/controller/AuthController.ts
@@ -74,15 +74,7 @@ export class AuthController extends AbstractController {
     const valid = userValidate(data);
 
     if (!valid) {
-      let errorMessage = '';
-
-      if (userValidate.errors) {
-        userValidate.errors?.forEach(errorObject => {
-          errorMessage += errorObject.message;
-        });
-      }
-
-      ctx.throw(errorMessage, 422);
+      ctx.throw(JSON.stringify(userValidate.errors), 422);
     }
 
     const loginname = data.loginname.toLowerCase();
--- test/api/controller/AuthController.test.ts
+++ test/api/controller/AuthController.test.ts
@@ -2,6 +2,14 @@ import assert from 'assert';
 import { Context } from 'egg';
 import { app } from 'egg-mock/bootstrap';
 
+const id = Date.now();
+
+const defaultUser = {
+  loginname: `cnodejs_${id}`,
+  email: `cnodejs_${id}@cnodejs.org`,
+  pass: 'cnodejs',
+};
+
 describe('test/app/api/controller/AuthController.test.ts', () => {
   let ctx: Context;
 
@@ -15,56 +23,27 @@ describe('test/app/api/controller/AuthController.test.ts', () => {
 
   describe('[POST /api/auth/signup]', () => {
     it('should get user info', async () => {
-
-      const user = {
-        loginname: 'cnodejs',
-        email: 'cnodejs@cnodejs.org',
-        pass: 'cnodejs',
-        rePass: 'cnodejs',
-      };
-
       const result = await app
         .httpRequest()
         .post('/api/auth/signup')
-        .send(user)
+        .send({
+          ...defaultUser,
+          rePass: defaultUser.pass,
+        })
         .expect(200);
 
-      assert(result.body.data.user.loginname, 'cnodejs');
+      assert(result.body.data.user.loginname === defaultUser.loginname);
     });
   });
 
   describe('[POST /api/auth/signin]', () => {
     it('should get user info', async () => {
-
-      const user = {
-        email: 'cnodejs@cnodejs.org',
-        pass: 'cnodejs',
-      };
-
       const result = await app
         .httpRequest()
         .post('/api/auth/signin')
-        .send(user)
-        .expect(200);
-
-      assert(result.body.data.user.active === false);
-    });
-
-
-  });
-
-  describe('[POST /api/auth/signin]', () => {
-    it('should get user info', async () => {
-
-      const user = {
-        loginname: 'cnodejs',
-        pass: 'cnodejs',
-      };
-
-      const result = await app
-        .httpRequest()
-        .post('/api/auth/signin')
-        .send(user)
+        .send({
+          ...defaultUser,
+        })
         .expect(200);
 
       assert(result.body.data.user.active === false);
